f4e139415e2496081434de764181a4836fc1bf55
keishaharris <keishadharriss@gmail.com>
2023-08-30 09:42:19 -0400

    Functionality for Link submission

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -3,39 +3,96 @@ import axios from 'axios'
 
 const Link = () => {
 
-//     const options = {
-//         method:'POST',
-//         url:'https://api.rebrandly.com/v1/links',
-//         headers: {
-//             accept: 'application/json',
-//             'content-type': 'application/json',
-//             apikey: '80656ef771314fc4a7d3ef2b316e554a',
-//         },
-//         data: {destination: 'https://www.google.com'}
-//     }
 
-//    async function getUrl (options){
+   async function shortenUrl (e){
+    e.preventDefault();
 
-//         const inputData = document.getElementsByName('input').value
+    const linkRequest = document.getElementById('inputValue').value;  
+   
+    const apiCall = {
+        method:'POST',
+        url:'https://api.rebrandly.com/v1/links',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            apikey: '80656ef771314fc4a7d3ef2b316e554a',
+        },
+        data: {destination: linkRequest}
+    }
 
-//         try{
-//             const response = await axios.request(options);
-//             console.log(response);
-//         } catch (err) {
-//             console.error(err);
-//         }
-//     }
 
+        try{
+            fetchUrl(apiCall).then((res) => {
+                console.log(res);
+              
+                const shortLink = res.shortUrl;
+                const oldLink = res.destination;
+                const body = document.querySelector('.link_collection');
+                
+                const div = document.createElement('div');
+                div.classList.add('clipboard');
+
+                const clipBody = document.createElement('div');
+                clipBody.classList.add('clip_body');
+
+                const p1 = document.createElement('p');
+                p1.innerText= `${oldLink}`
+
+                const clip_left = document.createElement('div');
+                clip_left.classList.add('clip_left');
+                clip_left.appendChild(p1);
+
+                const copyBtn = document.createElement('button');
+                copyBtn.innerText= 'Copy';
+
+                const p2 = document.createElement('p');
+                p2.innerText = `${shortLink}`;  
+
+                const clip_right = document.createElement('div');
+                clip_right.classList.add('clip_right');
+                clip_right.appendChild(p2);
+                clip_right.appendChild(copyBtn);
+
+                clipBody.appendChild(clip_left);
+                clipBody.appendChild(clip_right);
+
+                div.appendChild(clipBody);
+
+                body.appendChild(div);
+
+               
+
+            })
+            
+        } 
+        
+        
+        
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    async function fetchUrl(apiCall){
+        const data = await axios.request(apiCall);
+        const res = await data.data
+        return res;
+    }
 
     return (
+        <> 
         <div className="link_body"> 
         <div className='link_top'>
-                <input type="text" className="one" placeholder="Shorten a link here..." required/>
-                <button className="two sbmtbtn" > Shorten It!</button>
+                <input type="text" id="inputValue" className="one" name="url" placeholder="Shorten a link here..." required/>
+                <button type="submit" onClick={shortenUrl} className="two sbmtbtn" > Shorten It!</button>
             </div>
-                <div className='link_bottom'> Please add a link </div>
-            
+                {/* <div className='link_bottom'> Please add a link </div> */}
         </div>
+        <div className='link_collection'> 
+   
+        </div>
+          
+        </>
     )
 }
 export default Link
